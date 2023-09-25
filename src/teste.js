e1bcd0fe55f977fee3eca4cbd533b7e9875ece3e
Thiago Machado Pedro <94803123+ThiagoMachadoPedro@users.noreply.github.com>
2023-09-25 20:07:48 -0300

    teste realizado com sucesso

---
+++ src/teste.js
@@ -0,0 +1,13 @@
+"use strict";
+//multiple
+function multiplica(a, b) {
+    return a * b;
+}
+let multi = multiplica(100, 2);
+console.log(multi);
+//salute
+function ola(nome) {
+    return 'Olá ' + nome;
+}
+let result = ola('thiago');
+console.log(result);
---
+++ src/teste.ts
@@ -0,0 +1,15 @@
+//multiple
+function multiplica(a:number , b:number):number{
+  return a * b;
+}
+let multi =multiplica(100 ,2)
+console.log(multi);
+
+
+//salute
+function ola(nome: string){
+  return 'Olá ' + nome
+}
+
+let result = ola('thiago');
+console.log(result);
